7e025ff78db7b1711358caae855fa430260d4bf7
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2025-07-22 01:07:15 -0300

    teste ok

--- pwm.v
+++ pwm.v
@@ -1,11 +1,21 @@
 module PWM (
     input wire clk,
     input wire rst_n,
-    input wire [31:0] duty_cycle, // duty cycle final = duty_cycle / period
-    input wire [31:0] period, // pwm_freq = clk_freq / period
+    input wire [31:0] duty_cycle, 
+    input wire [31:0] period, 
     output reg pwm_out
 );
 
-//insira o seu código aqui
-    
+
+    reg [31:0] count;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count   <= 32'b0;
+            pwm_out <= 1'b0;
+        end else begin
+            pwm_out <= (count < duty_cycle)  ? 1'b1    : 1'b0;
+            count   <= (count < period-1)    ? count+1 : 32'b0;
+        end
+    end
 endmodule
--- servo.v
+++ servo.v
@@ -1,12 +1,85 @@
 module servo #(
-    parameter CLK_FREQ = 25_000_000, // 25 MHz
-    parameter PERIOD = 500_000 // 50 Hz (1/50s = 20ms, 25MHz / 50Hz = 500000 cycles)
+    parameter CLK_FREQ = 25_000_000, 
+    parameter PERIOD = 500_000 
 ) (
     input wire clk,
     input wire rst_n,
     output wire servo_out
 );
 
-//insira seu código aqui
+ 
+    localparam IDLE            = 2'b00;
+    localparam ESCURSAO_MINIMA = 2'b01;
+    localparam ESCURSAO_MAXIMA = 2'b10;
+
+    localparam integer CLK            = CLK_FREQ-1;
+    localparam integer PERIOD_PWM     = PERIOD;
+    localparam TEMPO_EXCURSAO_MIN_SEG = 5;
+    localparam TEMPO_EXCURSAO_MAX_SEG = 5;
+
+    reg [31:0] count;
+    reg [1:0] state, next_state;
+    reg [31:0] duty;
+
+    PWM pwm (
+        .clk(clk),
+        .rst_n(rst_n),
+        .duty_cycle(duty),
+        .period(PERIOD_PWM),
+        .pwm_out(servo_out)
+    );
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count <= 0;
+            state <= IDLE;
+        end else begin
+            state <= next_state;
+        end
+    end
+
+    always @(posedge clk) begin
+        case (state) 
+            IDLE: begin
+                next_state = ESCURSAO_MINIMA;
+            end
+
+            ESCURSAO_MINIMA: begin
+                if (count > CLK*TEMPO_EXCURSAO_MIN_SEG) begin
+                    count      <= 0;
+                    next_state <= ESCURSAO_MAXIMA;
+                end else begin
+                    count      <= count+1;
+                    next_state <= ESCURSAO_MINIMA;
+                end
+            end
+
+            ESCURSAO_MAXIMA: begin
+                if (count > CLK*TEMPO_EXCURSAO_MAX_SEG) begin
+                    count      <= 0;
+                    next_state <= ESCURSAO_MINIMA;
+                end else begin
+                    count      <= count+1;
+                    next_state <= ESCURSAO_MAXIMA;
+                end
+            end
+        endcase
+    end
+
+    always @(*) begin
+        case (state) 
+            IDLE: begin
+                duty <= 0;
+            end
+
+            ESCURSAO_MINIMA: begin
+                duty <= ((32'd5 * PERIOD_PWM) / 32'd100);
+            end
+
+            ESCURSAO_MAXIMA: begin
+                duty <= ((32'd10 * PERIOD_PWM) / 32'd100) + 2;
+            end
+        endcase
+    end
 
 endmodule
